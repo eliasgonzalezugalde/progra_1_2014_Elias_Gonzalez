18c0d5dacb89f6ecdce941d45f356c915c1ab08d
Elias. <eliasgonzalezugalde@gmail.com>
2014-05-15 21:20:27 -0600

    Commit Extra
    Archives's game

---
+++ code/extra/Game/src/game/Game.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package game;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Game extends JPanel {
+
+	Ball ball = new Ball(this);
+	Racquet racquet = new Racquet(this);
+
+	public Game() { //clase abstracta que implementa la interfaz listener
+		addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) { //le informamos a racket que alguien ha apretado una tecla
+				racquet.keyReleased(e); //Este se activa cuando alguien presiona una tecla
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				racquet.keyPressed(e);
+			}
+		});
+		setFocusable(true); //este game(que es un JPanel) se convierte en focusable para que pueda capturar eventos
+	}
+	
+	private void move() {
+		ball.move(); //le decimos a la bola que se mueva
+		racquet.move(); //le decimos a la raqueta que se mueva
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g); //limpiar el fondo (comentar para ver efecto)
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, //suavisar bola
+				RenderingHints.VALUE_ANTIALIAS_ON); //suavisar bola
+		ball.paint(g2d);
+		racquet.paint(g2d);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		JFrame frame = new JFrame("Mini Tennis"); //titulo de la ventana
+		Game game = new Game();
+		frame.add(game);
+		frame.setSize(300, 400); //tamaño de la ventana
+		frame.setVisible(true); //si esto no esta true, no aparece la ventana
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //para que aparezca la X de la ventana
+		
+		while (true) {
+			game.move(); //metodo que mueve la bola
+			game.repaint(); //metodo que repinta la bola
+			Thread.sleep(10);
+		}
+	}
+}
---
+++ code/extra/Game/src/game/Racquet.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package game;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+public class Racquet {
+	int x = 0;
+	int xa = 0; //velocidad del rectangulo
+	private Game game;
+
+	public Racquet(Game game) {
+		this.game= game;
+	}
+
+	public void move() {
+		if (x + xa > 0 && x + xa < game.getWidth()-60) //esto para evitar que la raqueta salga de la pantalla
+			x = x + xa;
+	}
+
+	public void paint(Graphics2D g) {
+		g.fillRect(x, 330, 60, 10); //rectangulo lleno, solo se mueve en el eje X, 60 de ancho, 10 de alto
+	}
+
+	public void keyReleased(KeyEvent e) {
+		xa = 0;
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) //si la tecla presionada es left mover la posición
+			xa = -1;
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			xa = 1;
+	}
+}
